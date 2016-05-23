122965aa19d103f4b203de24b10c896e3aa99a03
Thomas Bradford <thom@kode4food.it>
2016-05-23 14:13:51 +0200

    version bump

--- index.js
+++ index.js
@@ -1,44 +1,13 @@
-"use strict";
-
-const through = require('through2');
-
-const fatec = require('fatejs/dist/compiler/Compiler');
-const fatecli = require('fatejs/dist/cli/Compiler');
+const fate = require('fatejs');
 
 const pluginName = "gulp-fatejs";
 
-module.exports = function compiler() {
+function pluginInterface() {
 
-  return through.obj(function(file, encoding, callback) {
-    if ( file.isBuffer() ) {
-      processString(file.contents.toString());
-      return;
-    }
+}
 
-    if ( file.isStream() ) {
-      processStream(file.contents);
-      return;
-    }
+function fileProcessor(file, encoding, callback) {
+	
+}
 
-    callback(null, file);
-
-    function processStream(stream) {
-      let str = '';
-      file.contents.on('data', (data) => { str += data.toString(); });
-      file.contents.on('end', () => { processString(str); });
-    }
-
-    function processString(str) {
-      try {
-        let script = fatec.compileModule(str).scriptBody;
-        let nodeModule = fatecli.generateNodeModule(script);
-        file.contents = new Buffer(nodeModule);
-        callback(null, file);
-      }
-      catch ( err ) {
-        callback(err);
-      }
-    }
-  });
-
-};
+module.exports = pluginInterface;
